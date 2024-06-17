fbca8a7d76fd3dfbaacfc906d1bdf3be4947ec5a
Emanuel <emanuelnas29@gmail.com>
2024-06-17 09:54:35 -0300

    arquivos portfolio

---
+++ script.js
@@ -0,0 +1,63 @@
+/* ========================== toggle icon nav bar ================= */
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+} 
+
+/* ========================== scroll section active link ================= */
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
+    /* ========================== sticky navbar ================= */
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /* ========================== remove toggle icon and navbar ================= */
+
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
+
+/* ========================== scroll reveal ================= */
+
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .portfolio, .portfolio-box, .contato form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .sobre-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .sobre-content', { origin: 'right' });
+
+/* ========================== typed text ================= */
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Estudante de Eng. de Software', 'Full Stack Developer'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+});
